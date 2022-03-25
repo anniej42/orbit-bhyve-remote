98767287823ac7062a1987a7dfa67f8cc5eb4215
Yan Annie Jiao <yjiao@tesla.com>
2022-03-25 11:16:51 -0700

    Bump axios from 0.21.1 to 0.21.2
    Bumps [axios](https://github.com/axios/axios) from 0.21.1 to 0.21.2.
    - [Release notes](https://github.com/axios/axios/releases)
    - [Changelog](https://github.com/axios/axios/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/axios/axios/compare/v0.21.1...v0.21.2)
    
    ---
    updated-dependencies:
    - dependency-name: axios
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- server.js
+++ server.js
@@ -1,38 +1,56 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const fs = require('fs');
+const bodyParser = require("body-parser");
+const express = require("express");
+const fs = require("fs");
 
-const CONFIG_FILE = '/bhyve/config/config.json';
+const CONFIG_FILE = "/bhyve/config/config.json";
 
 const app = express();
 
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true,
-}));
+  })
+);
 app.use(bodyParser.json());
 
 if (!fs.existsSync(CONFIG_FILE)) {
-    app.use((req, res, next) => {
-        console.error("Can't complete request. Application config could not be loaded.");
-        next();
-    });
+  app.use((req, res, next) => {
+    console.error(
+      "Can't complete request. Application config could not be loaded."
+    );
+    next();
+  });
 } else {
-    const config = JSON.parse(fs.readFileSync(CONFIG_FILE));
+  const config = JSON.parse(fs.readFileSync(CONFIG_FILE));
 
-    // Require the correct token for each request
-    const { token } = config;
-    if (token) {
-        app.use((req, res, next) => {
-            const auth = req.body.token;
-            if (token === auth) {
-                next();
-            } else {
-                res.status(403).send();
-            }
-        });
-    }
+  // Require the correct token for each request
+  const { token } = config;
+  if (token) {
+    app.use((req, res, next) => {
+      const auth = req.body.token;
+      if (token === auth) {
+        next();
+      } else {
+        res.status(403).send();
+      }
+    });
+  }
 
-    app.use('/api', require('./src/routers/apiRouter')(config));
+  app.use("/api", require("./src/routers/apiRouter")(config));
 }
 
-app.listen(8080);
+// app.set("port", process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
+  console.log("Server is running.");
+});
+app
+  .get("/", function (request, response) {
+    var result = "App is running";
+    response.send(result);
+  })
+  .listen(app.get("port"), function () {
+    console.log(
+      "App is running, server is listening on port ",
+      app.get("port")
+    );
+  });
